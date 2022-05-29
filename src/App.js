cbe27cacf8a449e2aa00e84ee566c0a223b41fb0
Snidha19 <tasfiaahsan7@gmail.com>
2022-05-29 10:51:10 +0700

    add api call

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import './App.css';
+import axios from 'axios';
+import SearchBar from './components/SearchBar/index.component';
+import { useState } from 'react';
+
 
 function App() {
+  const [repos, setRepos] = useState([]);
+
+  const getApiData = async (props) => {
+    const response = await axios.get(`https://api.github.com/users/${props}/repos`);
+    setRepos(response.data);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <p className="header-text">
         List of Repositories
         </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
       </header>
+      <SearchBar onClickHandler={getApiData}/>
     </div>
   );
 }
