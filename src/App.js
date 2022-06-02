55fd59ef72361a12825d79940d4fea83c92eddfd
Snidha19 <tasfiaahsan7@gmail.com>
2022-06-02 12:26:11 +0700

    add searchBar and repoList to App layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import './App.css';
-import axios from 'axios';
-import SearchBar from './components/SearchBar/index.component';
-import { useState } from 'react';
-
+import React, { useRef, useState } from "react";
+import style from "./App.module.scss";
+import axios from "axios";
+import SearchBar from "./components/SearchBar/index.component";
+import RepositoryList from "./components/RepositoryList/index.component";
 
 function App() {
   const [repos, setRepos] = useState([]);
 
+  // const inputRef = useRef(null);
+
   const getApiData = async (props) => {
-    const response = await axios.get(`https://api.github.com/users/${props}/repos`);
-    setRepos(response.data);
+    try {
+
+      const response = await axios.get(
+        `https://api.github.com/users/${props}/repos`
+      );
+      setRepos(response);
+      
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p className="header-text">
-        List of Repositories
-        </p>
-      </header>
-      <SearchBar onClickHandler={getApiData}/>
+    <div className={style.App}>
+      <div className={style.App_header}>
+        <p className={style.header_text}>List of Repositories</p>
+      </div>
+
+      <div className={style.search_bar_container}>
+        <SearchBar onClickSearchButton={getApiData} />
+      </div>
+
+      <div className={style.repo_list_container}>
+        {repos.length !== 0 ? (
+          repos.data.map(item => (
+            <React.Fragment key={item.id}>
+              <RepositoryList repo={item} />
+            </React.Fragment>
+          ))
+        ) : (
+          <div> No repos found </div>
+        )}
+      </div>
     </div>
   );
 }
